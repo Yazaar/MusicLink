8d96426b86da1595cd590a80d4fd9bf851f6b0b6
Yazaar <Yazaar@users.noreply.github.com>
2020-01-28 22:35:09 +0100

    test

--- webconfig.py
+++
@@ -1,8 +0,0 @@
-'''
-CONFIG FILE FOR webhandler.py AND gunicorn
-
-launch command:
-gunicorn webhandler:app -c webconfig.py -k geventwebsocket.gunicorn.workers.GeventWebSocketWorker -w 1
-'''
-
-bind = '0.0.0.0:8000'
--- webhandler.py
+++ webhandler.py
@@ -1,22 +1,13 @@
-from flask import Flask
+from flask import Flask, request
 from flask_socketio import SocketIO
 
 app = Flask(__name__)
-socketio = SocketIO(app)
+sio = SocketIO(app)
 
 @app.route('/')
 def root():
-    return 'welcome!'
+    return 'welcome'
 
-@app.route('/<path:path>')
-def root(path):
-    return 'welcome! (' + path + ')'
-
-@socketio.on('join')
-def socketio_join(data):
-    return
-
-@socketio.on('message')
-def socketio_message(msg=''):
-    socketio.emit('message', msg.upper() + '!!!!!')
-    return
+@sio.on('message')
+def on_message(data=''):
+    sio.emit('message', 'hello', room=request.sio)
