414598f8b9701defadf80b97508324e1b2549506
Yazaar <41300736+Yazaar@users.noreply.github.com>
2020-01-31 12:22:57 +0100

    CORS

--- webhandler.py
+++ webhandler.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, render_template
 from flask_socketio import SocketIO
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 sio = SocketIO(app)
 
 @app.route('/')
