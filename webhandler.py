493c340b0aeca281815a4c0ddb3cf3a85ec01445
Yazaar <Yazaar@users.noreply.github.com>
2020-01-31 20:37:34 +0100

    huh please

--- webhandler.py
+++ webhandler.py
@@ -1,26 +1,13 @@
 import json
 from flask import Flask, request, render_template, redirect
 from flask_socketio import SocketIO
-from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
 sio = SocketIO(app)
-
-statusMessage = 'unset'
 
 @app.route('/', methods=['GET'])
 def root():
-    return render_template('index.html', statusMessage=statusMessage)
-
-@app.route('/setStatus', methods=['POST'])
-def setStatus():
-    global statusMessage
-    status = request.form.get('status')
-    if status != None:
-        statusMessage = status
-
-    return redirect('/', code=301)
+    return render_template('index.html')
 
 @sio.on('message')
 def on_message(data=''):
